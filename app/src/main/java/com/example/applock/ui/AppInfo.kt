fcde3dd4e0d243cc4fa8d913d91b0039caa54413
abusalahmohammadnasim <0.asmnasim@gmail.com>
2026-09-14 03:43:22 +0600

    Add AppInfo data class for application details

--- app/src/main/java/com/example/applock/ui/AppInfo.kt
+++ app/src/main/java/com/example/applock/ui/AppInfo.kt
@@ -1,1 +1,10 @@
+package com.example.applock.model
 
+import android.graphics.drawable.Drawable
+
+data class AppInfo(
+    val appName: String,
+    val packageName: String,
+    val icon: Drawable,
+    var isLocked: Boolean = false
+)
