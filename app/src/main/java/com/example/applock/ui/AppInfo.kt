630441d6a11cc5368ff092d255285a71513931f7
abusalahmohammadnasim <0.asmnasim@gmail.com>
2026-09-13 23:40:02 +0600

    Delete AppInfo data class
    Removed the AppInfo data class and its imports.

--- app/src/main/java/com/example/applock/ui/AppInfo.kt
+++ app/src/main/java/com/example/applock/ui/AppInfo.kt
@@ -1,10 +1,1 @@
-package com.example.applock.ui
 
-import android.graphics.drawable.Drawable
-
-data class AppInfo(
-    val appName: String,
-    val packageName: String,
-    val icon: Drawable,
-    var isLocked: Boolean
-)
